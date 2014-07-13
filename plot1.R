7ff0602ae7974ea78678379c5c55a2c7671f320f
tomhoi <hanhbk@yahoo.com>
2014-07-14 00:37:47 +0800

    second commit

--- plot1.R
+++ plot1.R
@@ -13,11 +13,11 @@ plot1 <- function(){
   # create a PNG file with a width of 480 pixels and a height of 480 pixels
   png("plot1.png", width=480, height=480)
 
-  hist(dataConsidered$Global_active_power,
-       main="Global Active Power",
-       xlab="Global Active Power (kilowatts)",
-       ylab="Frequency",
-       col="red")
+  hist( dataConsidered$Global_active_power,
+        main = "Global Active Power",
+        xlab = "Global Active Power (kilowatts)",
+        ylab = "Frequency",
+        col  = "red")
 
   dev.off()
 }
