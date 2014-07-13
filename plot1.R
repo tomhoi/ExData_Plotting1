b7e15c527b3aea1b0f0d294ff219c8c2e778b673
tomhoi <hanhbk@yahoo.com>
2014-07-14 00:03:20 +0800

    first commit

---
+++ plot1.R
@@ -0,0 +1,23 @@
+
+plot1 <- function(){
+  
+  dataFile <- "household_power_consumption.txt"
+  data <- read.table( dataFile, header=TRUE, sep=";", colClasses=c("character", "character", rep("numeric",7)), na="?" )
+  
+  data$Date <- as.Date( data$Date, "%d/%m/%Y" )
+  
+  datesConsidered <- as.Date( c("2007-02-01", "2007-02-02"), "%Y-%m-%d" )
+  
+  dataConsidered <- subset( data, Date %in% datesConsidered )
+
+  # create a PNG file with a width of 480 pixels and a height of 480 pixels
+  png("plot1.png", width=480, height=480)
+
+  hist(dataConsidered$Global_active_power,
+       main="Global Active Power",
+       xlab="Global Active Power (kilowatts)",
+       ylab="Frequency",
+       col="red")
+
+  dev.off()
+}
